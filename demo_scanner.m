9eed0c40edfa654984391e72f52d7315cfbc7fce
standard-lib <standard_lib@yahoo.co.jp>
2023-01-23 10:29:04 +0900

    USBメモリの中身をコピー．demoプログラムなので多分なにも問題ない．

--- demo_scanner.m
+++ demo_scanner.m
@@ -1,10 +1,13 @@
 clear all
 PS = MID_CV87x();
 
-PS.resetAddress('XYZR');
+%PS.resetAddress('XYZR');
 
-PS.setSpeed('Slow', 'XYZR');
-PS.driveAbs('X',30, 'Y', 30, 'Z', 5,'R', 30);
+% PS.setSpeed('Slow', 'XYZR');
+% PS.driveAbs('X',30, 'Y', 30, 'Z', 5,'R', 30);
 PS.queryAddress()
-PS.setSpeed('Fast', 'XYZR');
-PS.driveAbs('X', 0, 'Y',  0, 'Z', 0,'R', 0);
+% PS.setSpeed('Fast', 'XYZR');
+% PS.driveAbs('X', 0, 'Y',  0, 'Z', 0,'R', 0);
+PS.driveAbs('X', 0, 'Y',  0, 'Z', -100,'R', 0);
+PS.queryAddress()
+clear PS
